47f20f29aa344777663941bdea03cc58bb023fba
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-01-09 11:38:39 -0500

    First push of develop branch

--- BigMetric Watch App/Views/WeatherStatsView.swift
+++ BigMetric Watch App/Views/WeatherStatsView.swift
@@ -11,141 +11,151 @@ import CoreLocation
 import SwiftUI
 
 struct WeatherStatsView: View {
-   @ObservedObject var weatherKitManager: WeatherKitManager
-   @EnvironmentObject var geoCodeHelper: GeoCodeHelper
-   @EnvironmentObject var distanceTracker: DistanceTracker
-   @Binding var showWeatherStatsView: Bool
-   @State private var address = ""
-   var nextHrTemp: Double { Double(weatherKitManager.tempHour) ?? 0 }
-   var thisHrtemp: Double { Double(weatherKitManager.tempVar) ?? 0 }
-   var nextHrTempColor: Color { PrecipChanceColor.from(chance: Int(nextHrTemp)) }
-   var thisHrTempColor: Color { PrecipChanceColor.from(chance: Int(thisHrtemp)) }
-   var precipChance: Double { min(weatherKitManager.precipForecast * 100, 100) }
-   var precipColor: Color { PrecipChanceColor.from(chance: Int(precipChance)) }
+	@ObservedObject var weatherKitManager: WeatherKitManager
+	@EnvironmentObject var geoCodeHelper: GeoCodeHelper
+	@EnvironmentObject var distanceTracker: DistanceTracker
+	@Binding var showWeatherStatsView: Bool
+	@State private var address = ""
+	var hotColdFirst = true
+	var nextHrTemp: Double { Double(weatherKitManager.tempHour) ?? 0 }
+	var thisHrtemp: Double { Double(weatherKitManager.tempVar) ?? 0 }
+	var nextHrTempColor: Color { PrecipChanceColor.from(chance: Int(nextHrTemp)) }
+	var thisHrTempColor: Color { PrecipChanceColor.from(chance: Int(thisHrtemp)) }
+	var precipChance: Double { min(weatherKitManager.precipForecast * 100, 100) }
+	var precipColor: Color { PrecipChanceColor.from(chance: Int(precipChance)) }
 
-   var body: some View {
-      ScrollView {
-         VStack {
-            HStack {
-               Button(action: {
-                  showWeatherStatsView = false
-               }) {
-                  Text("") // Close
-               }
-               .font(.system(size: 9)) // Make the text smaller
-               .background(Color.clear) // Set the background to clear
-               .buttonStyle(PlainButtonStyle())
-               .cornerRadius(5)
-               .padding(.trailing)
-            }
+	var body: some View {
+		ScrollView {
+			VStack {
+				HStack {
+					Button(action: {
+						showWeatherStatsView = false
+					}) {
+						Text("") // Close
+					}
+					.font(.system(size: 9)) // Make the text smaller
+					.background(Color.clear) // Set the background to clear
+					.buttonStyle(PlainButtonStyle())
+					.cornerRadius(5)
+					.padding(.trailing)
+				}
 
-            Group {
-               // current temp & conditions
-               Text("\(weatherKitManager.tempVar)°")
-                  .font(.system(size: 50, weight: .bold))
-                  .foregroundColor(thisHrTempColor)
+				Group {
+					// current temp & conditions
+					Text("\(weatherKitManager.tempVar)°")
+						.font(.system(size: 50, weight: .bold))
+						.foregroundColor(thisHrTempColor)
 
-               Image(systemName: weatherKitManager.symbolVar)
-                  .font(.system(size: 45))
-                  .foregroundColor(.white)
-            }
-            .bold()
-            Spacer()
-            Group {
-               HStack {
-                  Text("\(weatherKitManager.lowTempVar)°")
-                     .foregroundColor(.gpBlue)
-                  Text(" / ")
-                     .foregroundColor(.white)
-                  Text("\(weatherKitManager.highTempVar)°")
-                     .foregroundColor(.gpRedPink)
-               }
-               .font(.system(size: 13))
+					Image(systemName: weatherKitManager.symbolVar)
+						.font(.system(size: 45))
+						.foregroundColor(.white)
+				}
+				.bold()
+				Spacer()
+				Group {
+					HStack {
+						Text("\(weatherKitManager.highTempVar)°")
+							.foregroundColor(TemperatureColor.from(temperature: Double(hotColdFirst ? weatherKitManager.highTempVar : weatherKitManager.lowTempVar)!))
+							.foregroundColor(.gpBlue)
+						Text(" / ")
+							.foregroundColor(.white)
+						Text("\(weatherKitManager.lowTempVar)°")
+							.foregroundColor(TemperatureColor.from(temperature: Double(hotColdFirst ? weatherKitManager.lowTempVar : weatherKitManager.highTempVar)!))
+					}
+					.font(.system(size: 13))
 
-               Image(systemName: weatherKitManager.symbolHourly)
-               HStack {
-                  Text("Next Hr Rain:")
-                     .foregroundColor(.white)
+					Image(systemName: weatherKitManager.symbolHourly)
+					HStack {
+						Text("Next Hr Rain:")
+							.foregroundColor(.white)
 
-                  Text("\(gpNumFormat.formatNumber(precipChance, 0))%")
-                     .foregroundColor(precipColor)
-               }
-               HStack {
-                  Text("Next Hr Temp:")
-                     .foregroundColor(.white)
+						Text("\(gpNumFormat.formatNumber(precipChance, 0))%")
+							.foregroundColor(precipColor)
+					}
+					HStack {
+						Text("Next Hr Temp:")
+							.foregroundColor(.white)
 
-                  Text(gpNumFormat.formatNumber(nextHrTemp, 0))
-                     .foregroundColor(nextHrTempColor)
-               }
-               //               }
-            }
-            .font(.system(size: 12))
-         }
+						Text(gpNumFormat.formatNumber(nextHrTemp, 0))
+							.foregroundColor(nextHrTempColor)
+					}
+					//               }
+				}
+				.font(.system(size: 12))
+			}
 
-         Text(address)
-            .onAppear {
-               geoCodeHelper.getCityNameHelper(distanceTracker.currentCoords.latitude,
-                                               distanceTracker.currentCoords.longitude) { result in
-                  address = result
-               }
-            }
-            .font(.footnote)
-            .foregroundColor(.gpPurple)
-            .bold()
+			Text(address)
+				.onAppear {
+					geoCodeHelper.getCityNameHelper(distanceTracker.currentCoords.latitude,
+															  distanceTracker.currentCoords.longitude) { result in
+						address = result
+					}
+				}
+				.font(.footnote)
+				.foregroundColor(.gpPurple)
+				.bold()
 
-         Divider()
-            .padding(.vertical)
-         /*
-          -------- create the list of daily weather view
-          */
-         VStack(alignment: .leading, spacing: 10) {
-            ForEach(Array(weatherKitManager.weekForecast.enumerated()), id: \.element.id) { index, forecast in
-               dailyView(index, forecast)
-               Divider()
-            }
-         }
-      }
-   }
-   
-//   func dailyView(_ index: Int, _ forecast: WeatherKitManager.Forecasts) -> HStack<TupleView<(Group<TupleView<(Text, Spacer, some View)>>, Spacer, some View)>> {
-//      return HStack {
-   func dailyView(_ index: Int, _ forecast: WeatherKitManager.Forecasts) -> some View {
-      HStack {
-         Group {
-            Text("\(gpDateStuff.getDayName(daysFromToday: index + 1)):")
-               .font(.system(size: 20))
-               .bold()
-               .foregroundColor(.white)
-            Spacer()
-            Image(systemName: forecast.symbolName)
-               .foregroundColor(.white)
-               .bold()
-         }
-         Spacer()
+			Divider()
+				.padding(.vertical)
+			/*
+			 -------- create the list of daily weather view
+			 */
+			VStack(alignment: .leading, spacing: 10) {
+				ForEach(Array(weatherKitManager.weekForecast.enumerated()), id: \.element.id) { index, forecast in
+					dailyView(index, forecast)
+					Divider()
+				}
+			}
+		}
+	}
 
-         HStack {
-            Text("\(forecast.minTemp)°")
-               .foregroundColor(TemperatureColor.from(temperature: Double(forecast.minTemp)!))
-               .font(.system(size: 17))
-            Text("/")
-               .foregroundColor(.white)
-            Text("\(forecast.maxTemp)°")
-               .foregroundColor(TemperatureColor.from(temperature: Double(forecast.maxTemp)!))
-               .font(.system(size: 17))
-         }
-         .font(.system(size: 20))
-         .bold()
-      }
-   }
-   //      .padding()
+	//   func dailyView(_ index: Int, _ forecast: WeatherKitManager.Forecasts) -> HStack<TupleView<(Group<TupleView<(Text, Spacer, some View)>>, Spacer, some View)>> {
+	//      return HStack {
+	func dailyView(_ index: Int, _ forecast: WeatherKitManager.Forecasts) -> some View {
+		HStack {
+			Group {
+				Text("\(gpDateStuff.getDayName(daysFromToday: index + 1)):")
+					.font(.system(size: 20))
+					.bold()
+					.foregroundColor(.white)
+				Spacer()
+				Image(systemName: forecast.symbolName)
+					.foregroundColor(.white)
+					.bold()
+			}
+			Spacer()
+
+			let mainTemp = hotColdFirst ? forecast.maxTemp : forecast.minTemp
+			let secondTemp = hotColdFirst ? forecast.minTemp : forecast.maxTemp
+			let mainTemperatureColor = TemperatureColor.from(temperature: Double(mainTemp)!)
+			let secondaryTemperatureColor = TemperatureColor.from(temperature: Double(secondTemp)!)
+
+			HStack {
+				Text("\(mainTemp)°")
+					.foregroundColor(mainTemperatureColor)
+					.font(.system(size: 17))
+
+				Text("/")
+					.foregroundColor(.white)
+
+				Text("\(secondTemp)°")
+					.foregroundColor(secondaryTemperatureColor)
+					.font(.system(size: 17))
+			}
+
+			.font(.system(size: 20))
+			.bold()
+		}
+	}
+	//      .padding()
 }
 
 struct WeatherStatsView_Previews: PreviewProvider {
-   static var previews: some View {
-      WeatherStatsView(weatherKitManager: WeatherKitManager(), showWeatherStatsView: .constant(true))
-         .environmentObject(WeatherKitManager())
-         .environmentObject(DistanceTracker())
-         .environmentObject(GeoCodeHelper())
-         .previewDisplayName("Debug Screen Preview")
-   }
+	static var previews: some View {
+		WeatherStatsView(weatherKitManager: WeatherKitManager(), showWeatherStatsView: .constant(true))
+			.environmentObject(WeatherKitManager())
+			.environmentObject(DistanceTracker())
+			.environmentObject(GeoCodeHelper())
+			.previewDisplayName("Debug Screen Preview")
+	}
 }
--- BigMetricApp.swift
+++ BigMetricApp.swift
@@ -6,6 +6,9 @@
 //    Modified: Modified: Sunday January 7, 2024 at 4:19:05 PM
 
 //			Trust ONLY this project 1/7/24 - 5:00 PM
+
+//			DEVELOP MODE branch
+
 //
 
 import SwiftUI
@@ -22,29 +25,6 @@ struct BigMetric_Watch_AppApp: App {
    @StateObject var weatherKitManager   = WeatherKitManager()
    @StateObject var geoCodeHelper       = GeoCodeHelper()
    @State private var distanceTrackerInitialized = false
-
-	/*	HOLY FUCKING COW! You know why...
-	 This is the root - @main view; the 'source of truth' so @StateObject is
-	 the parent - all other views needing access to the ParentObject need to use
-	 @ObservedObject or @EnvironmentObject
-
-	 @StateObject is the parent initializer for the object to be utilized in all child views.
-	 In order to be part of the "family", any child view requiring access to any of the components
-	 in the parent's need to use an ObservedObject or EnvironmentObject to "attach" to the parent's
-	 (and subsequent child) view components.
-
-	 @ObservedObject: This is used in child views that need to observe an object but don’t own it.
-	 You typically pass an ObservableObject that was created by a parent view (using @StateObject)
-	 to a child view and annotate it with @ObservedObject. This allows the child view to update
-	 when the observed object changes.
-
-	 @EnvironmentObject: This is another way to pass an observable object down the view hierarchy. Instead
-	 of passing it directly to child views, you inject it into the environment of a view hierarchy using
-	 .environmentObject(_:). Any view within this hierarchy can then access it by declaring it. It's
-	 useful for data that needs to be accessed by many views at different levels in the view hierarchy.
-
-	 The parent view initializes the child view with a modifier .environmentObject(observed-object-name) to share    */
-
    @State private var selectedTab = 2
 
    var body: some Scene {
